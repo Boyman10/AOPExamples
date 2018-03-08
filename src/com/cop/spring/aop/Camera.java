1402924168cac849aa99769c5f9d29fa61f691cc
Boyman10 <contact@yasi.fr>
2018-03-08 21:40:24 +0100

    Yet to fix an error after adding AOP feature

--- src/com/cop/spring/aop/Camera.java
+++ src/com/cop/spring/aop/Camera.java
@@ -2,6 +2,14 @@ package com.cop.spring.aop;
 
 public class Camera {
 
+	/**
+	 * NO arg constructor to help spring factory instantiate a bean
+	 */
+	public Camera() {
+		
+	}
+	
+	
 	public void snap() {
 		
 		System.out.println("SNAP");
--- src/com/cop/spring/aop/Logger.java
+++ src/com/cop/spring/aop/Logger.java
@@ -1,7 +1,22 @@
 package com.cop.spring.aop;
 
+/**
+ * This class is called an Aspect
+ * @author John
+ *
+ */
 public class Logger {
-
+	/**
+	 * NO arg constructor to help spring factory instantiate a bean
+	 */
+	public Logger() {
+		
+	}
+	
+	/*
+	 * This method advises
+	 * it is a join point
+	 */
 	public void aboutToTakePhoto() {
 		
 		// Should turn this class as an aspect to advice on the camera class..
